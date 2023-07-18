0ab9b4f55cbd5d436287714b921b3855f2ba0151
Jeff Langr <jeff@langrsoft.com>
2023-07-18 08:19:50 -0600

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/checkbook/CheckbookNumberConverter.java

--- src/test/java/checkbook/CheckbookNumberConverter.java
+++ src/test/java/checkbook/CheckbookNumberConverter.java
@@ -8,10 +8,21 @@ public class CheckbookNumberConverter {
       1, "one",
       7, "seven",
       10, "ten",
-      19, "nineteen",
-      20, "twenty"
+      19, "nineteen"
+   );
+
+   Map<Integer,String> tensConversions = Map.of(
+      2, "twenty"
    );
    public String convert(int number) {
+      if (number >= 20) {
+         var tensDigit = number / 10;
+         var remainder = number % 10;
+         var s = tensConversions.get(tensDigit);
+         if (remainder > 0)
+            s += "-" + conversions.get(remainder);
+         return s;
+      }
       return conversions.get(number);
    }
 }
